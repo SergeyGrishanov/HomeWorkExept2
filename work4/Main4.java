03b8215dd123d5aba0226bb9e587ed7ef00a435b
Sergey <blackwhiterainbow@yandex.ru>
2023-09-01 13:24:22 +0300

    Exeption пустая строка

---
+++ work4/Main4.java
@@ -0,0 +1,23 @@
+package work4;
+// Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. 
+// Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main4 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите любой текст кроме пустой строки");
+        try {
+            String result = reader.readLine();
+            if(result.equals(" ") || result.equals("")) 
+            throw new RuntimeException("Пустую строку вводить нельзя!");
+
+            System.out.println("Вы ввели: " + result + "\n Возвращайтесь к нам снова!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
